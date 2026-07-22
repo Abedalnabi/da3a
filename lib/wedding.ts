b685c8012a2b51bbdbc25f91eae8f004c29d175f
Mohammad abedalnabi <mohammad.abedalnabi@ics.jo>
2026-07-22 16:29:36 +0300

    Update contact placeholder with new phone number

--- lib/wedding.ts
+++ lib/wedding.ts
@@ -53,7 +53,7 @@ export const COPY = {
 
   notesTitle: "تنويهات",
   contactTitle: "للتواصل ",
-  contactPlaceholder: "0789814895",
+  contactPlaceholder: "079 044 3937",
 
   footerQuote: "حضوركم هديلُ فرحتنا... وتمامُ بهجتنا",
 } as const;
